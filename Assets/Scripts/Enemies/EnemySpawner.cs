5ef0bce348ac6ad380e4cdd1f3e83826a5d12cbb
R-Bleach <loljk366@gmail.com>
2025-03-03 15:38:38 +0100

    enemy added and start on warning sign

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,17 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class SpawnerScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static event Action<bool, bool> moving;
     float EnemyTimer = 10;
     float CoinTimer = 5;
     [SerializeField] EnemyInfo enemy;
     CoinInfo coins;
     List<float> prevPosEn = new List<float>();
     List<float> prevPosCo = new List<float>();
+    bool FirstSpawn;
+    Vector3 EnemSpawnPos;
+    int spawnChosen;
+    int chosenOrent;
     private void Start()
     {
         gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x * GameManager.Instance.scalingObj.localScale.x, gameObject.transform.localScale.y * GameManager.Instance.scalingObj.localScale.y, 0.1f);
@@ -31,6 +38,7 @@ public class SpawnerScript : MonoBehaviour
     {
         enemy = e;
         coins = coin;
+        FirstSpawn = true;
         StopAllCoroutines();
         StartCoroutine(SpawnEnemies());
         StartCoroutine(SpawnCoins());
@@ -40,15 +48,15 @@ public class SpawnerScript : MonoBehaviour
     {
         while (true)
         {
-            int spawnChoice = Random.Range(0, coins.CoinList.Count);
-            int randomSpawn = Random.Range(1, coins.CoinList[spawnChoice].ammountPerSpawn);
+            int spawnChoice = UnityEngine.Random.Range(0, coins.CoinList.Count);
+            int randomSpawn = UnityEngine.Random.Range(1, coins.CoinList[spawnChoice].ammountPerSpawn);
             print(randomSpawn);
             for (int j = 1; j > 0; j++)
             {
                 for (int i = 0; i < randomSpawn; i++)
                 {
                     float check = coins.CoinList[spawnChoice].CoinType.transform.localScale.x / 2;
-                    float randomX = Random.Range(transform.position.x - transform.localScale.x / 3, transform.position.x + transform.localScale.x / 3);
+                    float randomX = UnityEngine.Random.Range(transform.position.x - transform.localScale.x / 3, transform.position.x + transform.localScale.x / 3);
                     for (int k = 0; k < prevPosCo.Count; k++)
                     {
                         if (prevPosCo[k] > randomX - check && prevPosCo[k] < randomX + check)
@@ -64,7 +72,7 @@ public class SpawnerScript : MonoBehaviour
                     coin.GetComponent<CoinScript>().coin = coins;
                 }
                 prevPosCo.Clear();
-                CoinTimer = Random.Range(coins.minTimeBetweenSpawns, coins.maxTimeBetweenSpawns);
+                CoinTimer = UnityEngine.Random.Range(coins.minTimeBetweenSpawns, coins.maxTimeBetweenSpawns);
 
                 yield return new WaitForSeconds(CoinTimer);
             }
@@ -74,53 +82,56 @@ public class SpawnerScript : MonoBehaviour
     {
         while (true)
         {
-            int randomSpawn = Random.Range(1, enemy.AmmountPerSpawn);
+            int randomSpawn = UnityEngine.Random.Range(1, enemy.AmmountPerSpawn);
             print(randomSpawn);
-            for (int j = 1; j > 0; j++)
+            for (int i = 0; i < randomSpawn; i++)
             {
-                for (int i = 0; i < randomSpawn; i++)
+                if (!FirstSpawn)
                 {
-                    int spawnChoice = Random.Range(0, enemy.EnemyList.Count);
-                    float check = enemy.EnemyList[spawnChoice].EnemyType.transform.localScale.x / 2;
-                    float randomX = Random.Range(transform.position.x - transform.localScale.x / 3, transform.position.x + transform.localScale.x / 3);
-                    float randomY = Random.Range(transform.position.y + transform.localScale.y / 3, transform.position.y + transform.localScale.y / 3);
-                    for (int k = 0; k < prevPosEn.Count; k++)
+                    GameObject enem = Instantiate(enemy.EnemyList[spawnChosen].EnemyType, EnemSpawnPos, transform.rotation);
+                    enem.GetComponent<EnemyMoveBehaviour>().orientation = chosenOrent;
+                }
+                int spawnChoice = UnityEngine.Random.Range(0, enemy.EnemyList.Count);
+                float check = enemy.EnemyList[spawnChoice].EnemyType.transform.localScale.x / 2;
+                float randomX = UnityEngine.Random.Range(transform.position.x - transform.localScale.x / 3, transform.position.x + transform.localScale.x / 3);
+                float randomY = UnityEngine.Random.Range(transform.position.y + transform.localScale.y / 3, transform.position.y + transform.localScale.y / 3);
+                for (int k = 0; k < prevPosEn.Count; k++)
+                {
+                    if (prevPosEn[k] > randomX - check && prevPosEn[k] < randomX + check)
                     {
-                        if (prevPosEn[k] > randomX - check && prevPosEn[k] < randomX + check)
-                        {
-                            randomX += 1;
-                        }
+                        randomX += 1;
                     }
-                    prevPosEn.Add(randomX);
-                    int orient;
-                    Vector3 spawnPos;
-                    if (enemy.EnemyList[spawnChoice].SpawnTop)
+                }
+                prevPosEn.Add(randomX);
+                int orient;
+                Vector3 spawnPos;
+                if (enemy.EnemyList[spawnChoice].SpawnTop)
+                {
+                    spawnPos = new Vector3(randomX, transform.position.y + transform.localScale.y / 2, transform.position.z);
+                    orient = 1;
+                }
+                else
+                {
+                    int rand = UnityEngine.Random.Range(1, 3);
+                    if (rand == 1)
                     {
-                        spawnPos = new Vector3(randomX, transform.position.y + transform.localScale.y / 2, transform.position.z);
+                        spawnPos = new Vector3(transform.position.x + transform.localScale.x / 2, randomY, transform.position.z);
                         orient = 1;
                     }
                     else
                     {
-                        int rand = Random.Range(1, 3);
-                        if (rand == 1)
-                        {
-                            spawnPos = new Vector3(transform.position.x + transform.localScale.x / 2, randomY, transform.position.z);
-                            orient = 1;
-                        }
-                        else
-                        {
-                            spawnPos = new Vector3(transform.position.x - transform.localScale.x / 2, randomY, transform.position.z);
-                            orient = -1;
-                        }
-
+                        spawnPos = new Vector3(transform.position.x - transform.localScale.x / 2, randomY, transform.position.z);
+                        orient = -1;
                     }
-                    GameObject enem = Instantiate(enemy.EnemyList[spawnChoice].EnemyType, spawnPos, transform.rotation);
-                    enem.GetComponent<EnemyMoveBehaviour>().orientation = orient;
+
                 }
-                prevPosEn.Clear();
-                EnemyTimer = Random.Range(enemy.minTimeBetweenSpawns, enemy.maxTimeBetweenSpawns);
-                yield return new WaitForSeconds(EnemyTimer);
+                spawnChosen = spawnChoice;
+                EnemSpawnPos = spawnPos;
+                chosenOrent = orient;
             }
+            prevPosEn.Clear();
+            EnemyTimer = UnityEngine.Random.Range(enemy.minTimeBetweenSpawns, enemy.maxTimeBetweenSpawns);
+            if(!FirstSpawn) yield return new WaitForSeconds(EnemyTimer);
         }
     }
 }
--- Assets/Scripts/Enemies/UpBehaviour.cs
+++ Assets/Scripts/Enemies/UpBehaviour.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VultureMoveBehaviour : EnemyMoveBehaviour
+public class UpMoveBehaviour : EnemyMoveBehaviour
 {
     // Start is called before the first frame updateee;
     [SerializeField] float MoveSpeed;
-    [SerializeField] float fallSpeed;
+    [SerializeField] float riseSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(new Vector2(-MoveSpeed  * orientation, -GameManager.Instance.speed / fallSpeed));
+        rb.AddForce(new Vector2(-MoveSpeed * orientation, GameManager.Instance.speed / riseSpeed));
         if (cam.WorldToScreenPoint(transform.position).x < -Screen.width / 1.5 || cam.WorldToScreenPoint(transform.position).x > Screen.width * 2)
         {
-
             Destroy(gameObject);
         }
     }
